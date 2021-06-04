94c9bfb2f8cb303b6a94ddabaa0de9fdf8ceb215
HARDTACK-Dev <rjsdn827@gmail.com>
2021-06-04 23:41:56 +0900

    Update Input_dump Analyzer .py

--- Input_dump Analyzer .py
+++ Input_dump Analyzer .py
@@ -3,7 +3,7 @@
 print("Input Dumper")
 print("------------")
 
-file_path = input("Choose File (path):")
+file_path = input("Choose File (path):\n")
 
 if file_path.endswith(".txt") is False:
     print("\nSorry, It's only support .txt files")
@@ -14,13 +14,22 @@ with open(file_path, 'r') as file:
     lines = [lines.strip() for lines in raw_lines]
 
     accel_duration = {}  # number of press up / values
+    a1 = []
 
-    # search for press up keywords
+    # search for press up keywords, get accelerated seconds
     for i in range(len(lines)):
         if "press up" in lines[i]:
             accel_duration[i] = lines[i].split("-")
 
-            pressUp_start = accel_duration[i][0]
-            pressUp_end = accel_duration[i][1][:-9]
+            pressUp_start = int(accel_duration[i][0])
+            pressUp_end = int(accel_duration[i][1][:-9])
 
-            print(pressUp_start + " " + pressUp_end)
+            print("[Acceleration]: " + str((pressUp_end - pressUp_start) / 1000) + "s")
+
+
+            a1[i] = str((pressUp_end - pressUp_start)/1000)
+
+            pressUp_start = 0
+            pressUp_end = 0
+
+    print("[Release]: " + str(a1[1] - a1[0]) + "s")
